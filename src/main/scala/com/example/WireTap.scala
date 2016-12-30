937b181c8d37f5e01ba7d913a58547d8d7bf0713
Michihito Shigemura <m_shigemura@shigemk2.com>
2016-12-31 04:07:05 +0900

    Add main

--- src/main/scala/com/example/WireTap.scala
+++ src/main/scala/com/example/WireTap.scala
@@ -3,6 +3,14 @@ package com.example
 import akka.actor._
 
 object WireTapDriver extends CompletableApp(2) {
+  val order = Order("123")
+  val orderProcessor = system.actorOf(Props[OrderProcessor], "orderProcessor")
+  val logger= system.actorOf(Props[MessageLogger], "logger")
+  val orderProcessorWireTap = logger
+
+  orderProcessorWireTap ! ProcessOrder(order)
+  awaitCompletion
+  println(s"WireTap is completed.")
 }
 
 class MessageLogger(messageReceiver: ActorRef) extends Actor {
